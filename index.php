c5c088ae269cc883171c31b016a306eaa36f3e78
MonMed <monicameduri1@gmail.com>
2023-05-11 02:22:15 -0700

    Input param method

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
 </head>
 <body>
 	<div class="container">
-		<form action="s_login.php" method="post" id="login-form" autocomplete="off">
+		<form action="s_login_inputParam.php" method="post" id="login-form" autocomplete="off">
 			<h2>Login Form</h2>
 			<label for="username">Username:</label>
 			<input type="text" id="username" name="username" required>
---
+++ s_login_inputParam.php
@@ -0,0 +1,78 @@
+<?php   
+    include("config.php");
+
+    if(!isset($_SESSION)) 
+    { 
+        $session = session_start();
+    } 
+
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        // username and password sent from form 
+
+        // $username = mysqli_real_escape_string($db,$_POST['username']);
+        // $password = mysqli_real_escape_string($db,$_POST['password']); 
+        
+        // Input categorization
+        $username = $_POST['username'];
+        $password = $_POST['password']; 
+        $keywords = array("OR", "UNION", "SELECT");
+        $specialChars = array("/", ",", "$", "(", ")", "\"", "//", "?", "+", "!", "@", "=", "'");
+        $alphabets = range('a', 'z');
+        $alphabets = array_merge($alphabets, range('A', 'Z'));
+        $numbers = range(0, 9);
+        
+        // Check if input is safe using input verifier
+        $isSafe = true;
+        $inputSet = array(str_split($username), str_split($password));
+        foreach ($inputSet as $input) {
+            foreach ($input as $char) {
+                if (in_array($char, $keywords) || in_array($char, $specialChars)) {
+                    $isSafe = false;
+                    break;
+                }
+                else if (!in_array($char, $alphabets) && !in_array($char, $numbers)) {
+                    $isSafe = false;
+                    break;
+                }
+            }
+        }
+        
+        if ($isSafe) {
+            $sql = "SELECT id FROM users WHERE username = '$username' and password = '$password'";
+
+            $result = mysqli_query($db,$sql);
+
+            $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+            if ($row != NULL) {
+
+                $active = $row['id'];
+
+                $count = mysqli_num_rows($result);
+                // If result matched $myusername and $mypassword, table row must be 1 row
+
+                $sql = "SELECT firstname, username FROM users WHERE username = '$username' and password = '$password'";
+                $result = mysqli_query($db,$sql);
+
+                $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+                $displayName = $row['firstname'];
+                $loginName = $row['username'];
+
+                if($count >= 1) {
+                    $_SESSION['login_user'] = $loginName;
+                    $_SESSION['display_name'] = $displayName;
+                    $_SESSION['id'] = $active;
+                    header("location: user_account.php");
+                } else {
+                    echo "Your Login Name or Password is invalid";
+                }
+            } else {
+                echo "Your Username or Password is invalid";
+            }
+        }
+        else {
+            echo "Invalid input. Please try again.";
+        }
+    }
+?>
